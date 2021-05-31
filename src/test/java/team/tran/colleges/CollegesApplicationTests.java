a72c913cb42b48c03ee950d37e0b29792494c525
yulu-999 <66932309+yulu-999@users.noreply.github.com>
2021-05-31 17:02:06 +0800

    测试修改

---
+++ src/test/java/team/tran/colleges/CollegesApplicationTests.java
@@ -0,0 +1,18 @@
+package team.tran.colleges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @className: CollegesApplicationTests
+ * @description: TODO 类描述
+ * @author: tran
+ * @date: 2021/5/31
+ **/
+@SpringBootTest
+public class CollegesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
