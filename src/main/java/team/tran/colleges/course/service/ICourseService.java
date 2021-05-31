5cb32d9fccd1e64eef9323567a8d8e87832dfc99
TianCaiShaoNianQinMou <2441708149@qq.com>
2021-05-31 19:59:15 +0800

    提出建议和课程搜索的接口

--- src/main/java/team/tran/colleges/course/service/ICourseService.java
+++ src/main/java/team/tran/colleges/course/service/ICourseService.java
@@ -1,5 +1,7 @@
 package team.tran.colleges.course.service;
 
+import java.util.Map;
+
 /**
  * @className: ICourseService
  * @description: TODO 课程热榜 课程搜索
@@ -7,4 +9,23 @@ package team.tran.colleges.course.service;
  * @date: 2021/5/31
  **/
 public interface ICourseService {
+
+    /**
+     * 根据条件模糊查询
+     * @param page 页数
+     * @param size 条数
+     * @param msg 条件
+     * @return Map
+     */
+    Map<String,Object> LikeCourse(Integer page,Integer size,String msg);
+
+
+    /**
+     * 查询全部
+     * @param page 页数
+     * @param size 条数
+     * @return Map
+     */
+    Map<String ,Object> selectCourse(Integer page,Integer size);
+
 }
--- src/main/java/team/tran/colleges/suggest/service/ISuggestService.java
+++ src/main/java/team/tran/colleges/suggest/service/ISuggestService.java
@@ -1,5 +1,7 @@
 package team.tran.colleges.suggest.service;
 
+import java.util.Map;
+
 /**
  * @className: ISuggestService
  * @description: TODO 提出宝贵的建议
@@ -7,4 +9,13 @@ package team.tran.colleges.suggest.service;
  * @date: 2021/5/31
  **/
 public interface ISuggestService {
+
+    /**
+     * 提出建议
+     * @param token 学生的token
+     * @param suggestMsg 提出的建议
+     * @param id 课程id
+     * @return Map
+     */
+    Map<String ,Object> addSuggest(String token,String suggestMsg,String id);
 }
