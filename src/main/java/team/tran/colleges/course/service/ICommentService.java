0c120efb1ddb155a1771d8a1e18803ec090ef058
yulu-999 <66932309+yulu-999@users.noreply.github.com>
2021-05-31 17:03:24 +0800

    测试提交

---
+++ src/main/java/team/tran/colleges/course/service/ICommentService.java
@@ -0,0 +1,29 @@
+package team.tran.colleges.course.service;
+
+import java.util.Map;
+
+/**
+ * @className: ICommentService
+ * @description: TODO 课程点评
+ * @author: tran
+ * @date: 2021/5/31
+ **/
+public interface ICommentService {
+
+
+    /**
+     * @param: token  学生的 token
+     * @param: id 课程的id
+     * @param: grade 评级  1 ：优 2 ：好  3 : 一般
+     * @description: TODO 学生对课程经行评级
+     * @return: java.util.Map<java.lang.String,java.lang.Object>
+     * @author: tran
+     * @date: 2021/5/31
+     */
+    Map<String, Object> stuRemark(String token,String id,Integer grade);
+
+
+
+
+
+}
